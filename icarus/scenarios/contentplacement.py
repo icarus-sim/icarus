614e610e8b77ca92bca8d7b1f3843c52a2510695
Lorenzo Saino <l.saino@ucl.ac.uk>
2013-11-28 21:18:45 +0000

    Add weighted content placement
    This commit adds a function that implements a weighted content
    placment, i.e. it assigns content objects to servers randomly
    whereby the probability of a content being assigned to a specific
    server is proportional to a user-provided server weight

--- icarus/scenarios/contentplacement.py
+++ icarus/scenarios/contentplacement.py
@@ -9,7 +9,7 @@ import collections
 from fnss.util import random_from_pdf
 
 
-__all__ = ['uniform_content_placement']
+__all__ = ['uniform_content_placement', 'weighted_content_placement']
 
 
 def uniform_content_placement(topology, contents, source_nodes, seed=None):
@@ -40,3 +40,37 @@ def uniform_content_placement(topology, contents, source_nodes, seed=None):
     for c in contents:
         content_placement[random.choice(source_nodes)].append(c)
     return content_placement
+    
+def weighted_content_placement(topology, contents, source_weights, seed=None):
+    """Places content objects to source nodes randomly according to the weight
+    of the source node.
+    
+    Parameters
+    ----------
+    topology : Topology
+        The topology object
+   contents : list
+        List of content objects
+    source_weights : dict
+        Dict mapping nodes nodes of the topology which are content sources and
+        the weight according to which content placement decision is made.
+        
+    Returns
+    -------
+    cache_placement : dict
+        Dictionary mapping content objects to source nodes
+
+    
+    Notes
+    -----
+    A deterministic placement of objects (e.g., for reproducing results) can be
+    achieved by using a fix seed value
+    """
+    random.seed(seed)
+    norm_factor = float(sum(source_weights.values()))
+    source_pdf = dict((k, v/norm_factor) for k, v in source_weights.iteritems())
+    content_placement = collections.defaultdict(list)
+    for c in contents:
+        content_placement[random_from_pdf(source_pdf)].append(c)
+    return content_placement
+    
