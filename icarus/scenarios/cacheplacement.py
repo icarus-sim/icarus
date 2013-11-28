6403642d8da42e5d017f622cd1cb3dbda54665ae
Lorenzo Saino <l.saino@ucl.ac.uk>
2013-11-28 22:01:19 +0000

    Add degree-based cache placement
    This commits adds a function that assigns cache space to cache nodes
    proportionally to the degree centrality of the node

--- icarus/scenarios/cacheplacement.py
+++ icarus/scenarios/cacheplacement.py
@@ -6,6 +6,7 @@ import networkx as nx
 
 __all__ = [
         'uniform_cache_placement',
+        'degree_centrality_cache_placement',
           ]
 
 
@@ -28,3 +29,25 @@ def uniform_cache_placement(topology, cache_budget, cache_nodes, **kwargs):
     """
     cache_size = int(cache_budget//len(cache_nodes))
     return dict((v, cache_size) for v in cache_nodes)
+
+
+def degree_centrality_cache_placement(topology, cache_budget, cache_nodes, **kwargs):
+    """Places cache budget proportionally to the degree of the node.
+    
+    Parameters
+    ----------
+    topology : Topology
+        The topology object
+    cache_budget : int
+        The cumulative cache budget
+    cache_nodes : list
+        List of nodes of the topology on which caches can be deployed
+        
+    Returns
+    -------
+    cache_placement : dict
+        Dictionary mapping node to assigned cache space
+    """
+    deg = nx.degree(topology)
+    total_deg = sum(deg.values())
+    return dict((v, int(cache_budget*deg[v]//total_deg)) for v in cache_nodes)
