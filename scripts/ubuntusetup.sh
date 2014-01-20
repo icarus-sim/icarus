d4c18164ed421d8147ebaf4f112d48bd35f4c675
Lorenzo Saino <l.saino@ucl.ac.uk>
2014-01-20 01:10:28 +0000

    Add script to install all Icarus dependencies on Ubuntu

---
+++ scripts/ubuntusetup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# This script installs all Icarus dependencies
+#
+# It has been tested successfully only on Ubuntu 13.10+.
+#
+sudo apt-get install python ipython python-pip python-numpy python-scipy python-matplotlib python-nose  
+sudo pip install -U networkx fnss
