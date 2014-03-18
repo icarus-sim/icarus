a857a4b9467c34b2fc1b239cfa7e9d517aeac17d
Lorenzo Saino <l.saino@ucl.ac.uk>
2014-03-18 14:03:25 +0000

    Add more test cases for cache performance modelling tools
    This commit adds test cases for the Lautaris cache hit ratio and the
    optimal cache hit ratio calculators.

--- icarus/tools/tests/test_cacheperf.py
+++ icarus/tools/tests/test_cacheperf.py
@@ -35,3 +35,19 @@ class TestNumericCacheHitRatio(unittest.TestCase):
         pass
 
 
+class TestLaoutarisCacheHitRatio(unittest.TestCase):
+    
+    def test_3rd_order_positive_disc(self):
+        h = cacheperf.laoutaris_cache_hit_ratio(0.8, 1000, 100, 3)
+        self.assertGreaterEqual(h, 0)
+        
+    def test_3rd_order_negative_disc(self):
+        h = cacheperf.laoutaris_cache_hit_ratio(0.7, 1000, 100, 3)
+        self.assertGreaterEqual(h, 0)
+        
+
+class TestOptimalCacheHitRatio(unittest.TestCase):
+    
+    def test_unsorted_pdf(self):
+        h = cacheperf.optimal_cache_hit_ratio([0.1, 0.5, 0.4], 2)
+        self.assertAlmostEqual(0.9, h)
