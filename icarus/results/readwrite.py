icarus-sim/icarus

commit 5066ee674942c5a70efb1926bac272372a78e80c

Minor improvements to ResultSet class
This commit changes the signature of the constructor and the add
method to simplify its usage.

icarus/results/readwrite.py
@@ -24,12 +24,17 @@ class ResultSet(object):
     be shared by different processes.
     """
     
-    def __init__(self):
+    def __init__(self, attr=None):
         """Constructor
+        
+        Parameters
+        ----------
+        attr : dict, optional
+            Dictionary of common attributes to all experiments
         """
         self._results = collections.deque()
         # Dict of global attributes common to all experiments
-        self.attr = {}
+        self.attr = attr if attr else {}
     
     def __len__(self):
         """Returns the number of results in the resultset
@@ -83,17 +88,17 @@ class ResultSet(object):
             rs.add(i)
         return rs
 
-    def add(self, result):
-        """Add a result to the result set. This method is thread safe
+    def add(self, parameters, results):
+        """Add a result to the result set.
         
         Parameters
         ----------
-        results : 2-value tuple
-            2-value tuples where the first value is a dictionary of experiment
-            parameters and the second value is the dictionary of experiment
-            results
+        parameters : dict
+            Dictionary of experiment parameters
+        results : dict
+            Dictionary of experiment results
         """
-        self._results.append(result)
+        self._results.append((parameters, results))
     
     def dump(self):
         """Dump all results.

icarus/orchestration.py
@@ -141,7 +141,7 @@ class Orchestrator(object):
         # Extract parameters
         params, results, seq, duration = args
         # Store results
-        self.results.add((params, results))
+        self.results.add(params, results)
         self.exp_durations.append(duration)
         if seq % self.summary_freq == 0:
             # Number of experiments scheduled to be executed

icarus/results/tests/test_resultset.py
@@ -19,9 +19,9 @@ class TestResultSet(unittest.TestCase):
         cls.cond_b = {'alpha': 1, 'beta': 2, 'gamma': 3}
         cls.cond_c = {'alpha': -1, 'beta': -2, 'gamma': 3}
         cls.metric = {'m1': 1, 'm2': 2, 'm3': 3}
-        cls.rs.add((cls.cond_a, cls.metric))
-        cls.rs.add((cls.cond_b, cls.metric))
-        cls.rs.add((cls.cond_c, cls.metric))
+        cls.rs.add(cls.cond_a, cls.metric)
+        cls.rs.add(cls.cond_b, cls.metric)
+        cls.rs.add(cls.cond_c, cls.metric)
         pass
         
     @classmethod

commit 26e70d9081bbf3e4b61a6450b40816cc07afa0e8

Fix minor bug in ResultSet
Fix a bug introduced by 5066ee674942c5a70efb1926bac272372a78e80c

icarus/results/readwrite.py
@@ -86,7 +86,7 @@ class ResultSet(object):
                              'they have different global attributes')
         rs = copy.deepcopy(self)
         for i in iter(resultset):
-            rs.add(i)
+            rs.add(*i)
         return rs
 
     def add(self, parameters, results):