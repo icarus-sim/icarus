0e2a1a3d493dc792a98c3365f17d5b7a3beb6dc6
Lorenzo Saino <l.saino@ucl.ac.uk>
2014-06-12 13:44:38 +0100

    Install NetworkX from Ubuntu repos in ubuntusetup.sh

--- scripts/ubuntusetup.sh
+++ scripts/ubuntusetup.sh
@@ -4,5 +4,5 @@
 #
 # It has been tested successfully only on Ubuntu 13.10+.
 #
-sudo apt-get install python ipython python-pip python-numpy python-scipy python-matplotlib python-nose python-sphinx
-sudo pip install -U networkx fnss numpydoc
+sudo apt-get -y -q install python ipython python-pip python-numpy python-scipy python-matplotlib python-nose python-sphinx python-networkx
+sudo pip install -U fnss numpydoc
