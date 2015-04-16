ac267bee5279e1868cebd8871804d772648b21fa
Lorenzo Saino <l.saino@ucl.ac.uk>
2015-04-16 09:13:02 +0100

    Add test case for weighted content placement

--- icarus/scenarios/test/test_contentplacement.py
+++ icarus/scenarios/test/test_contentplacement.py
@@ -12,7 +12,6 @@ import fnss
 import icarus.scenarios as contentplacement
 
 
-
 class TestUniform(unittest.TestCase):
 
     @classmethod
@@ -39,3 +38,32 @@ class TestUniform(unittest.TestCase):
         c1 = t.node[1]['stack'][1]['contents']
         c2 = t.node[2]['stack'][1]['contents']
         self.assertEqual(len(c1) + len(c2), 10)
+        
+class TestWeighted(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass    
+    
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_uniform(self):
+        t = fnss.line_topology(4)
+        fnss.add_stack(t, 0, 'router')
+        fnss.add_stack(t, 1, 'source')
+        fnss.add_stack(t, 2, 'source')
+        fnss.add_stack(t, 3, 'receiver')
+        contentplacement.weighted_content_placement(t, range(10), {1: 0.7, 2: 0.3})
+        c1 = t.node[1]['stack'][1]['contents']
+        c2 = t.node[2]['stack'][1]['contents']
+        self.assertEqual(len(c1) + len(c2), 10)
+        
+    
