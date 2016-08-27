d9ec659a62d243c3ce2e83f46d34e3b7608299f1
Lorenzo Saino <l.saino@ucl.ac.uk>
2016-08-27 09:10:00 +0100

    Move all test cases to py.test
    All test cases used nosetest which is no longer actively developed. For
    this reason it was advisable to move to py.test, which is actively
    maintained.

--- test.py
+++ test.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python
-"""Run unit tests
-"""
+"""Run unit tests"""
 from os import path, getcwd
 
-__all__ = ['run']
-
-def run(verbosity=1):
-    """Run tests.
-
-    Parameters
-    ----------
-    verbosity: integer, optional
-      Level of detail in test reports.  Higher numbers provide more detail.  
-
-    """
+def main():
+    """Run all tests"""
     try:
-        import nose
+        import pytest
     except ImportError:
-        raise ImportError("The nose package is needed to run the tests.")
+        raise ImportError("The pytest package is needed to run the tests.")
+
     # get folder of Python source files
     src_dir = path.join(path.dirname(__file__), path.pardir)
 
     # stop if running from source directory
     if getcwd() == path.abspath(path.join(src_dir, path.pardir)):
         raise RuntimeError("Can't run tests from source directory.\n"
-                           "Run 'nosetests' from the command line.")
+                           "Run 'py.test' from the command line.")
     # Run tests
-    nose.run()
-    
-if __name__=="__main__":
-    run()
+    pytest.main(['icarus'])
+
+if __name__ == "__main__":
+    main()
 
