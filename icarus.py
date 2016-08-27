720f5b8742a10bb3da599e6bd754b50b0b9eb6a8
Lorenzo Saino <l.saino@ucl.ac.uk>
2016-08-27 09:12:00 +0100

    Revamp icarus.py script

--- icarus.py
+++ icarus.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
-"""This script runs the Icarus simulator.
+"""Run the Icarus simulator.
 
-It automatically adds Icarus source folder to the PYTHONPATH and then
-executes the simulator according to the settings specified in the provided 
+This script automatically adds Icarus source folder to the PYTHONPATH and then
+executes the simulator according to the settings specified in the provided
 configuration file.
 """
 import sys
-from os import path
+import os
 import argparse
 
-
 def main():
-    src_dir = path.abspath(path.dirname(__file__))
+    src_dir = os.path.abspath(os.path.dirname(__file__))
     sys.path.insert(0, src_dir)
     from icarus import __version__
     from icarus.run import run
@@ -25,12 +24,11 @@ def main():
     parser.add_argument("config",
                         help="configuration file")
     parser.add_argument('-v', '--version', action='version',
-                        version="icarus %s" % __version__)
+                        version="icarus {}".format(__version__))
     args = parser.parse_args()
     config_override = dict(c.split("=") for c in args.config_override) \
-             if args.config_override else None
+                      if args.config_override else None
     run(args.config, args.results, config_override)
-
 
 if __name__ == "__main__":
     main()
