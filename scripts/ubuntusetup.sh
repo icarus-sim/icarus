9118ed128c9b7c6c448719cb5e1c6e70f366d5e3
Lorenzo Saino <l.saino@ucl.ac.uk>
2016-10-16 00:50:42 +0100

    Remove obsolete scripts

--- scripts/ubuntusetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# This script installs all Icarus dependencies
-#
-# It has been tested successfully only on Ubuntu 13.10+.
-#
-sudo apt-get -y -q install python ipython python-pip python-numpy python-scipy python-matplotlib python-nose python-sphinx python-networkx
-sudo pip install -U fnss numpydoc
