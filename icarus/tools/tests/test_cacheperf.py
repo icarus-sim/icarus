9cce82c44b20ab14ff968a24d42c16543029d650
Lorenzo Saino <l.saino@ucl.ac.uk>
2019-07-14 15:59:39 +0100

    Improve coverage of test cases for characteristic time approximations

--- icarus/tools/tests/test_cacheperf.py
+++ icarus/tools/tests/test_cacheperf.py
@@ -49,15 +49,21 @@ class TestLaoutarisPerContentCacheHitRatio(unittest.TestCase):
 
     def test_3rd_order_positive_disc(self):
         H = cacheperf.laoutaris_per_content_cache_hit_ratio(0.8, 1000, 100, 3)
+        prev_h = 1
         for h in H:
             self.assertGreaterEqual(h, 0)
             self.assertLessEqual(h, 1)
+            self.assertLessEqual(h, prev_h)
+            prev_h = h
 
     def test_3rd_order_negative_disc(self):
         H = cacheperf.laoutaris_per_content_cache_hit_ratio(0.7, 1000, 100, 3)
+        prev_h = 1
         for h in H:
             self.assertGreaterEqual(h, 0)
             self.assertLessEqual(h, 1)
+            self.assertLessEqual(h, prev_h)
+            prev_h = h
 
 
 class TestFaginApproximation(unittest.TestCase):
@@ -93,6 +99,8 @@ class TestFaginApproximation(unittest.TestCase):
 
 class TestCheApproximation(unittest.TestCase):
 
+    # Numerically evaluated cache hit ratio for alpha=0.8, n=100 and c=40 is ~ 0.64
+
     @classmethod
     def setUpClass(cls):
         cls.pdf = stats.TruncatedZipfDist(alpha=0.8, n=100).pdf
@@ -100,34 +108,65 @@ class TestCheApproximation(unittest.TestCase):
 
     def test_che_characteristic_time(self):
         T = cacheperf.che_characteristic_time(self.pdf, self.cache_size)
+        prev_t = np.infty
         for t in T:
             self.assertGreaterEqual(t, self.cache_size)
+            self.assertLessEqual(t, prev_t)
+            prev_t = t
 
     def test_che_characteristic_time_simplified(self):
         t = cacheperf.che_characteristic_time_simplified(self.pdf, self.cache_size)
         self.assertGreaterEqual(t, self.cache_size)
+        self.assertGreaterEqual(t, 72.1)
+        self.assertLessEqual(t, 72.3)
+
+    def test_che_characteristic_time_generalized(self):
+        t = cacheperf.che_characteristic_time_generalized(self.pdf, self.cache_size, "LRU")
+        self.assertGreaterEqual(t, self.cache_size)
+        self.assertGreaterEqual(t, 72.1)
+        self.assertLessEqual(t, 72.3)
 
     def test_che_cache_hit_ratio(self):
         h = cacheperf.che_cache_hit_ratio(self.pdf, self.cache_size)
-        self.assertGreaterEqual(h, 0)
-        self.assertLessEqual(h, 1)
+        self.assertGreaterEqual(h, 0.63)
+        self.assertLessEqual(h, 0.66)
 
     def test_che_cache_hit_ratio_simplified(self):
         h = cacheperf.che_cache_hit_ratio_simplified(self.pdf, self.cache_size)
-        self.assertGreaterEqual(h, 0)
-        self.assertLessEqual(h, 1)
+        self.assertGreaterEqual(h, 0.63)
+        self.assertLessEqual(h, 0.66)
+
+    def test_che_cache_hit_ratio_generalized(self):
+        h = cacheperf.che_cache_hit_ratio_generalized(self.pdf, self.cache_size, "LRU")
+        self.assertGreaterEqual(h, 0.63)
+        self.assertLessEqual(h, 0.66)
 
     def test_che_per_content_cache_hit_ratio(self):
         H = cacheperf.che_per_content_cache_hit_ratio(self.pdf, self.cache_size)
+        prev_h = 1
         for h in H:
             self.assertGreaterEqual(h, 0)
             self.assertLessEqual(h, 1)
+            self.assertLessEqual(h, prev_h)
+            prev_h = h
 
     def test_che_per_content_cache_hit_ratio_simplified(self):
         H = cacheperf.che_per_content_cache_hit_ratio_simplified(self.pdf, self.cache_size)
+        prev_h = 1
         for h in H:
             self.assertGreaterEqual(h, 0)
             self.assertLessEqual(h, 1)
+            self.assertLessEqual(h, prev_h)
+            prev_h = h
+
+    def test_che_per_content_cache_hit_ratio_generalized(self):
+        H = cacheperf.che_per_content_cache_hit_ratio_generalized(self.pdf, self.cache_size, "LRU")
+        prev_h = 1
+        for h in H:
+            self.assertGreaterEqual(h, 0)
+            self.assertLessEqual(h, 1)
+            self.assertLessEqual(h, prev_h)
+            prev_h = h
 
 
 class TestLaoutarisCacheHitRatio(unittest.TestCase):
